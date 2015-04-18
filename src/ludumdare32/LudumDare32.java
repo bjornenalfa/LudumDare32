c172f322173944334c46713fc465a6ad38c1fe55
Letfik <buggi@live.se>
2015-04-18 12:54:35 +0200

    basic jframe template

--- src/ludumdare32/LudumDare32.java
+++ src/ludumdare32/LudumDare32.java
@@ -1,21 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ludumdare32;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 /**
  *
  * @author bjornenalfa
  */
-public class LudumDare32 {
+public class LudumDare32 extends JFrame {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public LudumDare32() {
+        setTitle("");
+
+        MyPanel panel = new MyPanel();
+
+        setContentPane(panel);
+        getContentPane().setPreferredSize(new Dimension(800, 600));
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
-    
+
+    class MyPanel extends JPanel {
+
+        public MyPanel() {
+            addKeyBindings();
+
+            MouseAdapter ma = mouseAdapter();
+            addMouseListener(ma);
+            addMouseMotionListener(ma);
+            addMouseWheelListener(ma);
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+
+        }
+
+        private void addKeyBindings() {
+            getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_0, 0), "keyBinding");
+            getActionMap().put("keyBinding", keyBinding());
+        }
+
+        private Action keyBinding() {
+            return new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                }
+            };
+        }
+
+        private MouseAdapter mouseAdapter() {
+            return new MouseAdapter() {
+                @Override
+                public void mouseReleased(MouseEvent me) {
+
+                }
+            };
+        }
+    }
+
+    public static void main(String[] args) {
+        new LudumDare32();
+    }
 }
