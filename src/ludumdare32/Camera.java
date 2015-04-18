24b4d260d41df20994e13358ab92f212511c20e2
Letfik <buggi@live.se>
2015-04-18 17:11:34 +0200

    camera

--- src/ludumdare32/Camera.java
+++ src/ludumdare32/Camera.java
@@ -1,5 +1,36 @@
 package ludumdare32;
 
+import ludumdare32.LudumDare32.MyPanel;
+
 public class Camera {
-    
+
+    double translateX, translateY;
+    int halfWidth, halfHeight;
+
+    MyPanel panel;
+
+    public Camera(MyPanel panel) {
+        halfWidth = panel.getWidth() / 2;
+        halfHeight = panel.getHeight() / 2;
+    }
+
+    public void update(Player player, boolean centered) {
+        if (centered) {
+            translateX = -player.getX() - halfWidth;
+            if (translateX < halfWidth) {
+                translateX = halfWidth;
+            }
+            if (translateX > World.width - halfWidth) {
+                translateX = World.width - halfWidth;
+            }
+            translateY = -player.getY() - halfHeight;
+            if (translateY < halfHeight) {
+                translateY = halfHeight;
+            }
+            if (translateY > World.height - halfHeight) {
+                translateY = World.height - halfHeight;
+            }
+        }
+        
+    }
 }
