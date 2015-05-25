7e79c565585a3c1cda812c3f6f7572c19b749be3
bjornenalfa <bjorn.detterfelt@telia.com>
2015-05-25 14:41:52 +0200

    all the collisions!

--- LudumDare32MapEditor/src/mapeditor/CollisionPanel.java
+++ LudumDare32MapEditor/src/mapeditor/CollisionPanel.java
@@ -23,35 +23,38 @@ public class CollisionPanel extends JPanel {
         }
     }
 
-    int s = 20;
-    int r = 5;
+    int squareSize = 20;
+    int glowRadius = 5;
 
     private void addButton(int i) {
-        BufferedImage img = new BufferedImage(30, 30, BufferedImage.TYPE_INT_ARGB);
+        int width = squareSize+2*glowRadius;
+        int rsq = glowRadius*glowRadius;
+        
+        BufferedImage img = new BufferedImage(width, width, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = (Graphics2D) img.getGraphics();
         g.setColor(Color.LIGHT_GRAY);
-        g.fillRect(0, 0, s + 2 * r, s + 2 * r);
+        g.fillRect(0, 0, width, width);
 
-        for (int x = 0; x < (s + 2 * r); x++) {
-            for (int y = 0; y < (s + 2 * r); y++) {
-                double cx = x - r;
-                double cy = y - r;
+        for (int x = 0; x < (width); x++) {
+            for (int y = 0; y < (width); y++) {
+                double cx = x - glowRadius;
+                double cy = y - glowRadius;
                 if (i == 1) {// BIT 1 = BASE SQUARE COLLISION
-                    cx = Math.max(0, Math.min(31, cx));
-                    cy = Math.max(0, Math.min(31, cy));
+                    cx = Math.max(0, Math.min(squareSize-1, cx));
+                    cy = Math.max(0, Math.min(squareSize-1, cy));
                 }
                 if (i == 2) {// BIT 2 = TOP LEFT NO COLLISION
-                    if (cx + cy < 32) {
+                    if (cx + cy < squareSize) {
                         // Ortogonal projektion av vektor från (0,32) till c på normaliserad vektor (1,-1)
-                        cy = cy - 32;
+                        cy = cy - squareSize;
                         double scalar = (cx / Math.sqrt(2) - cy / Math.sqrt(2)); // Skalär produkt mellan c och normaliserad vektor (1,-1)
                         cx = scalar / Math.sqrt(2); // c = length * normaliserad vektor (1,-1)
                         cy = -scalar / Math.sqrt(2);
-                        cy = cy + 32;
+                        cy = cy + squareSize;
                     }
                 }
                 if (i == 3) {// BIT 3 = TOP RIGHT NO COLLISION
-                    if ((32 - cx) + cy < 32) {
+                    if ((squareSize - cx) + cy < squareSize) {
                         // Ortogonal projektion av vektor från (0,0) till c på normaliserad vektor (1,1)
                         double scalar = (cx / Math.sqrt(2) + cy / Math.sqrt(2)); // Skalär produkt mellan c och normaliserad vektor (1,1)
                         cx = scalar / Math.sqrt(2); // c = length * normaliserad vektor (1,1)
@@ -59,7 +62,7 @@ public class CollisionPanel extends JPanel {
                     }
                 }
                 if (i == 4) {// BIT 4 = BOTTOM LEFT NO COLLISION
-                    if (cx + (32 - cy) < 32) {
+                    if (cx + (squareSize - cy) < squareSize) {
                         // Ortogonal projektion av vektor från (0,0) till c på normaliserad vektor (1,1)
                         double scalar = (cx / Math.sqrt(2) + cy / Math.sqrt(2)); // Skalär produkt mellan c och normaliserad vektor (1,1)
                         cx = scalar / Math.sqrt(2); // c = length * normaliserad vektor (1,1)
@@ -67,39 +70,78 @@ public class CollisionPanel extends JPanel {
                     }
                 }
                 if (i == 5) {// BIT 5 = BOTTOM RIGHT NO COLLISION
-                    if ((32 - cx) + (32 - cy) < 32) {
+                    if ((squareSize - cx) + (squareSize - cy) < squareSize) {
                         // Ortogonal projektion av vektor från (0,32) till c på normaliserad vektor (1,-1)
-                        cy = cy - 32;
+                        cy = cy - squareSize;
                         double scalar = (cx / Math.sqrt(2) - cy / Math.sqrt(2)); // Skalär produkt mellan c och normaliserad vektor (1,-1)
                         cx = scalar / Math.sqrt(2); // c = length * normaliserad vektor (1,-1)
                         cy = -scalar / Math.sqrt(2);
-                        cy = cy + 32;
+                        cy = cy + squareSize;
                     }
                 }
                 if (i == 6) {// BIT 6 = LEFT EDGE NO COLLISION
+                    cx = Math.max(0, Math.min(width, cx));
                     if (cx < 1) {
+                        Point.Double collisionVector = new Point.Double(x - cx - glowRadius, y - cy - glowRadius);
+                        double sqDist = collisionVector.x * collisionVector.x + collisionVector.y * collisionVector.y;
+                        if (sqDist < rsq) {
+                            if (sqDist == 0) {
+                                g.setColor(new Color(255, 255, 255, 64));
+                            } else {
+                                //int k = (int) (255*(Math.sqrt(sqDist)/(16)));
+                                int k = (int) (255 * (sqDist / (rsq)));
+                                g.setColor(new Color(255, 255-k, 255-k, 255 - k));
+                            }
+                            g.fillRect(x, y, 1, 1);
+                        }
                         cx = 100000;
                     }
                 }
                 if (i == 7) {// BIT 7 = RIGHT EDGE NO COLLISION
-                    if (cx > 30) {
+                    cx = Math.max(-glowRadius, Math.min(squareSize-1, cx));
+                    if (cx > squareSize-2) {
+                        Point.Double collisionVector = new Point.Double(x - cx - glowRadius, y - cy - glowRadius);
+                        double sqDist = collisionVector.x * collisionVector.x + collisionVector.y * collisionVector.y;
+                        if (sqDist < rsq) {
+                            if (sqDist == 0) {
+                                g.setColor(new Color(255, 255, 255, 64));
+                            } else {
+                                //int k = (int) (255*(Math.sqrt(sqDist)/(16)));
+                                int k = (int) (255 * (sqDist / (rsq)));
+                                g.setColor(new Color(255, 255-k, 255-k, 255 - k));
+                            }
+                            g.fillRect(x, y, 1, 1);
+                        }
                         cx = 100000;
                     }
                 }
                 if (i == 8) {// BIT 8 = TOP EDGE NO COLLISION
-                    if (cy > 30) {
+                    cy = Math.max(0, Math.min(width, cy));
+                    if (cy < 1) {
+                        Point.Double collisionVector = new Point.Double(x - cx - glowRadius, y - cy - glowRadius);
+                        double sqDist = collisionVector.x * collisionVector.x + collisionVector.y * collisionVector.y;
+                        if (sqDist < rsq) {
+                            if (sqDist == 0) {
+                                g.setColor(new Color(255, 255, 255, 64));
+                            } else {
+                                //int k = (int) (255*(Math.sqrt(sqDist)/(16)));
+                                int k = (int) (255 * (sqDist / (rsq)));
+                                g.setColor(new Color(255, 255-k, 255-k, 255 - k));
+                            }
+                            g.fillRect(x, y, 1, 1);
+                        }
                         cy = 100000;
                     }
                 }
-                Point.Double collisionVector = new Point.Double(x - cx - r, y - cy - r);
+                Point.Double collisionVector = new Point.Double(x - cx - glowRadius, y - cy - glowRadius);
                 double sqDist = collisionVector.x * collisionVector.x + collisionVector.y * collisionVector.y;
-                if (sqDist < 16 * 16) {
+                if (sqDist < rsq) {
                     if (sqDist == 0) {
-                        g.setColor(new Color(255, 255, 255, 32));
+                        g.setColor(new Color(255, 255, 255, 64));
                     } else {
                         //int k = (int) (255*(Math.sqrt(sqDist)/(16)));
-                        int k = (int) (255 * (sqDist / (16 * 16)));
-                        g.setColor(new Color(255 - k, 255, 255, 255 - k));
+                        int k = (int) (255 * (sqDist / (rsq)));
+                        g.setColor(new Color(255-k, 255, 255, 255 - k));
                     }
                     g.fillRect(x, y, 1, 1);
                 }
@@ -114,7 +156,7 @@ public class CollisionPanel extends JPanel {
 
             }
         });
-        button.setPreferredSize(new Dimension(30, 30));
+        button.setPreferredSize(new Dimension(width, width));
         add(button);
     }
 
