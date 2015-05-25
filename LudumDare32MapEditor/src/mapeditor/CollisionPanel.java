b2e73d5003867cf349c9441f63385a373e9ac68a
bjornenalfa <bjorn.detterfelt@telia.com>
2015-05-25 15:11:57 +0200

    :frog:

--- LudumDare32MapEditor/src/mapeditor/CollisionPanel.java
+++ LudumDare32MapEditor/src/mapeditor/CollisionPanel.java
@@ -29,7 +29,7 @@ public class CollisionPanel extends JPanel {
 
     private void addButtons() {
         for (int i = 0; i < 8; i++) {
-            addButton(i + 1);
+            addButton(i);
         }
     }
 
@@ -37,6 +37,42 @@ public class CollisionPanel extends JPanel {
     int glowRadius = 5;
 
     private void addButton(int i) {
+        JButton button = new JButton(new ImageIcon(getImage(1<<i,squareSize,glowRadius,false)));
+        button.setFocusPainted(false);
+        final int ID = i;
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                if ((result & (1 << (ID-1))) == (1 << (ID-1))) {
+//                    result -= Math.pow(2, ID-1);
+//                } else {
+//                    result += Math.pow(2, ID-1);
+//                }
+                pressed(ID);
+            }
+        });
+        buttons[i] = button;
+        button.setPreferredSize(new Dimension(squareSize+2*glowRadius, squareSize+2*glowRadius));
+        add(button);
+    }
+
+    private void pressed(int ID) {
+        selected[ID] = !selected[ID];
+        buttons[ID].setIcon(new ImageIcon(getImage(1<<ID,squareSize,glowRadius,selected[ID])));
+        updatePreview();
+    }
+
+    public void updatePreview() {
+        result = 0;
+        for (int i = 0;i<selected.length;i++) {
+            if (selected[i]) {
+                result += 1<<i;
+            }
+        }
+        preview.setIcon(new ImageIcon(getImage(result,45,10,false)));
+    }
+    
+    public BufferedImage getImage(int i, int squareSize, int glowRadius, boolean selected) {
         int width = squareSize+2*glowRadius;
         int rsq = glowRadius*glowRadius;
         
@@ -44,16 +80,20 @@ public class CollisionPanel extends JPanel {
         Graphics2D g = (Graphics2D) img.getGraphics();
         g.setColor(Color.LIGHT_GRAY);
         g.fillRect(0, 0, width, width);
+        if (selected) {
+            g.setColor(new Color(70,255,70,70));
+            g.fillRect(0, 0, width, width);
+        }
 
         for (int x = 0; x < (width); x++) {
             for (int y = 0; y < (width); y++) {
                 double cx = x - glowRadius;
                 double cy = y - glowRadius;
-                if (i == 1) {// BIT 1 = BASE SQUARE COLLISION
+                if ((i & 1) == 1) {// BIT 1 = BASE SQUARE COLLISION
                     cx = Math.max(0, Math.min(squareSize-1, cx));
                     cy = Math.max(0, Math.min(squareSize-1, cy));
                 }
-                if (i == 2) {// BIT 2 = TOP LEFT NO COLLISION
+                if ((i & 2) == 2) {// BIT 2 = TOP LEFT NO COLLISION
                     if (cx + cy < squareSize) {
                         // Ortogonal projektion av vektor från (0,32) till c på normaliserad vektor (1,-1)
                         cy = cy - squareSize;
@@ -63,7 +103,7 @@ public class CollisionPanel extends JPanel {
                         cy = cy + squareSize;
                     }
                 }
-                if (i == 3) {// BIT 3 = TOP RIGHT NO COLLISION
+                if ((i & 4) == 4) {// BIT 3 = TOP RIGHT NO COLLISION
                     if ((squareSize - cx) + cy < squareSize) {
                         // Ortogonal projektion av vektor från (0,0) till c på normaliserad vektor (1,1)
                         double scalar = (cx / Math.sqrt(2) + cy / Math.sqrt(2)); // Skalär produkt mellan c och normaliserad vektor (1,1)
@@ -71,7 +111,7 @@ public class CollisionPanel extends JPanel {
                         cy = scalar / Math.sqrt(2);
                     }
                 }
-                if (i == 4) {// BIT 4 = BOTTOM LEFT NO COLLISION
+                if ((i & 8) == 8) {// BIT 4 = BOTTOM LEFT NO COLLISION
                     if (cx + (squareSize - cy) < squareSize) {
                         // Ortogonal projektion av vektor från (0,0) till c på normaliserad vektor (1,1)
                         double scalar = (cx / Math.sqrt(2) + cy / Math.sqrt(2)); // Skalär produkt mellan c och normaliserad vektor (1,1)
@@ -79,7 +119,7 @@ public class CollisionPanel extends JPanel {
                         cy = scalar / Math.sqrt(2);
                     }
                 }
-                if (i == 5) {// BIT 5 = BOTTOM RIGHT NO COLLISION
+                if ((i & 16) == 16) {// BIT 5 = BOTTOM RIGHT NO COLLISION
                     if ((squareSize - cx) + (squareSize - cy) < squareSize) {
                         // Ortogonal projektion av vektor från (0,32) till c på normaliserad vektor (1,-1)
                         cy = cy - squareSize;
@@ -89,7 +129,7 @@ public class CollisionPanel extends JPanel {
                         cy = cy + squareSize;
                     }
                 }
-                if (i == 6) {// BIT 6 = LEFT EDGE NO COLLISION
+                if ((i & 32) == 32) {// BIT 6 = LEFT EDGE NO COLLISION
                     cx = Math.max(0, Math.min(width, cx));
                     if (cx < 1) {
                         Point.Double collisionVector = new Point.Double(x - cx - glowRadius, y - cy - glowRadius);
@@ -107,7 +147,7 @@ public class CollisionPanel extends JPanel {
                         cx = 100000;
                     }
                 }
-                if (i == 7) {// BIT 7 = RIGHT EDGE NO COLLISION
+                if ((i & 64) == 64) {// BIT 7 = RIGHT EDGE NO COLLISION
                     cx = Math.max(-glowRadius, Math.min(squareSize-1, cx));
                     if (cx > squareSize-2) {
                         Point.Double collisionVector = new Point.Double(x - cx - glowRadius, y - cy - glowRadius);
@@ -125,7 +165,7 @@ public class CollisionPanel extends JPanel {
                         cx = 100000;
                     }
                 }
-                if (i == 8) {// BIT 8 = TOP EDGE NO COLLISION
+                if ((i & 128) == 128) {// BIT 8 = TOP EDGE NO COLLISION
                     cy = Math.max(0, Math.min(width, cy));
                     if (cy < 1) {
                         Point.Double collisionVector = new Point.Double(x - cx - glowRadius, y - cy - glowRadius);
@@ -157,37 +197,6 @@ public class CollisionPanel extends JPanel {
                 }
             }
         }
-
-        JButton button = new JButton(new ImageIcon(img));
-        button.setFocusPainted(false);
-        final int ID = i;
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-//                if ((result & (1 << (ID-1))) == (1 << (ID-1))) {
-//                    result -= Math.pow(2, ID-1);
-//                } else {
-//                    result += Math.pow(2, ID-1);
-//                }
-                pressed(ID);
-            }
-        });
-        button.setPreferredSize(new Dimension(30, 30));
-        buttons[i - 1] = button;
-        button.setPreferredSize(new Dimension(width, width));
-        add(button);
-    }
-
-    private void pressed(int ID) {
-        selected[ID] = !selected[ID];
-        
-    }
-
-    public static void updatePreview() {
-        BufferedImage img = new BufferedImage(32, 32, BufferedImage.TYPE_INT_ARGB);
-        Graphics g = img.getGraphics();
-        g.setColor(Color.MAGENTA);
-        g.fillRect(0, 0, 32, 32);
-        preview.setIcon(new ImageIcon(img));
+        return img;
     }
 }
