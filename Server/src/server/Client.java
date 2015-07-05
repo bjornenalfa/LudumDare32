aa5eb8e9611e0ecf29690f0f9580b14deac22449
pastapojken <dariorosto@yahoo.it>
2015-07-05 17:02:17 +0200

    Server & Client no more GUI

--- Server/src/server/Client.java
+++ Server/src/server/Client.java
@@ -5,18 +5,6 @@
  */
 package server;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowStateListener;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.EOFException;
@@ -27,29 +15,12 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.nio.charset.Charset;
-import java.security.SecureRandom;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.KeyStroke;
-import javax.swing.SwingConstants;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.text.DefaultCaret;
 
 /**
  *
@@ -62,282 +33,29 @@ public class Client implements Runnable {
     private String myID, srvIP;
     private boolean running, reset, connected, valIn;
     private Thread InThread, OutThread;
-    private JFrame startFr, conFr, mainFr;
-    private JPanel startP;
-    private JTextArea outF, inArea;
-    private JButton sendMsg;
 
-    public Client() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        conFr = new JFrame();
-        srvPort = 9002;
-        inJFrame();
+    public Client(String ip, int port, String id) {
+        srvIP = ip;
+        srvPort = port;
+        myID = id;
         reset = false;
         connected = false;
-    }
-
-    private void start() {
-        boolean disconnect = true;
-        while (disconnect) {
-            try {
-                srvSocket = new DatagramSocket();
-                disconnect = false;
-            } catch (IllegalArgumentException ex) {
-                disconnect = true;
-                startFr.dispose();
-                conFr.dispose();
-                final JDialog d = new JDialog(startFr, "", true);
-                d.setLayout(new FlowLayout());
-                JButton but = new JButton("OK");
-                but.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e1) {
-                        d.dispose();
-                    }
-                });
-                JLabel str = new JLabel("Can not reach the server, please enter another ip!", SwingConstants.CENTER);
-                d.setLocationRelativeTo(null);
-                d.setResizable(false);
-                d.add(str);
-                d.add(but);
-                d.pack();
-                d.setVisible(true);
-                inJFrame();
-            } catch (IOException ex) {
-                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-
-    private void inJFrame() {
-        valIn = false;
-        startFr = new JFrame();
-        startFr.setResizable(false);
-        startFr.setLocationRelativeTo(null);
-        KeyStroke escapeKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false);
-        Action escapeAction = new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                startFr.dispatchEvent(new WindowEvent(startFr, WindowEvent.WINDOW_CLOSING));
-            }
-        };
-        startFr.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escapeKeyStroke, "ESCAPE");
-        startFr.getRootPane().getActionMap().put("ESCAPE", escapeAction);
-        startP = new JPanel();
-        JPanel pan1 = new JPanel();
-        JPanel pan2 = new JPanel();
-        JPanel pan3 = new JPanel();
-        startFr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        startFr.setSize(325, 200);
-        startFr.setLayout(new GridLayout());
-        final JTextField srvIPF = new JTextField(srvIP, 25);
-        JLabel str1 = new JLabel("Server ip:");
-        final JTextField srvIPp = new JTextField(Integer.toString(srvPort), 25);
-        JLabel str2 = new JLabel("Server port:");
-        final JTextField usrN = new JTextField(myID, 25);
-        JLabel str3 = new JLabel("Username:");
-        JButton button1 = new JButton("Connect");
-        JButton button2 = new JButton("Exit");
-
-        pan1.add(str1);
-        pan1.add(srvIPF);
-        pan2.add(str2);
-        pan2.add(srvIPp);
-        pan3.add(str3);
-        pan3.add(usrN);
-        startP.add(pan1);
-        startP.add(pan2);
-        startP.add(pan3);
-        startP.add(button1);
-        startP.add(button2);
-        startFr.add(startP);
-        startFr.setVisible(true);
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                srvIP = srvIPF.getText();
-                String input = srvIPp.getText();
-                if (input.matches("[0-9]+")) {
-                    srvPort = Integer.parseInt(input);
-                    valIn = true;
-                    myID = usrN.getText();
-                    if (myID.isEmpty()) {
-                        valIn = false;
-                        final JDialog d = new JDialog(startFr, "", true);
-                        d.setLayout(new BorderLayout());
-                        JButton but = new JButton("OK");
-                        but.addActionListener(new ActionListener() {
-                            @Override
-                            public void actionPerformed(ActionEvent e1) {
-                                d.dispose();
-                            }
-                        });
-                        JLabel str = new JLabel("Username can not be empty!", SwingConstants.CENTER);
-                        d.setLocationRelativeTo(startFr);
-                        d.setResizable(false);
-                        d.add(str, BorderLayout.NORTH);
-                        d.add(but, BorderLayout.SOUTH);
-                        d.pack();
-                        d.setVisible(true);
-                    } else {
-                        valIn = true;
-                    }
-                } else {
-                    valIn = false;
-                    final JDialog d = new JDialog(startFr, "", true);
-                    d.setLayout(new BorderLayout());
-                    JButton but = new JButton("OK");
-                    but.addActionListener(new ActionListener() {
-                        @Override
-                        public void actionPerformed(ActionEvent e1) {
-                            d.dispose();
-                        }
-                    });
-                    JLabel str = new JLabel("Invalid port!", SwingConstants.CENTER);
-                    d.setLocationRelativeTo(startFr);
-                    d.setResizable(false);
-                    d.add(str, BorderLayout.NORTH);
-                    d.add(but, BorderLayout.SOUTH);
-                    d.pack();
-                    d.setVisible(true);
-                }
-            }
-        });
-        button2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                startFr.dispatchEvent(new WindowEvent(startFr, WindowEvent.WINDOW_CLOSING));
-            }
-        });
-        while (!valIn) {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException ex) {
-            }
-        }
-    }
-
-    private void reset() {
-        if (!InThread.isInterrupted()) {
-            InThread.interrupt();
-            try {
-                InThread.join();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        if (!OutThread.isInterrupted()) {
-            OutThread.interrupt();
-            try {
-                OutThread.join();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        reset = false;
-        connected = false;
-        mainFr.dispose();
-        inJFrame();
-        this.run();
     }
 
     @Override
     public void run() {
-        startFr.dispose();
-        conFr.setLocationRelativeTo(null);
-        conFr.setResizable(false);
-        conFr.add(new JLabel("Connecting...", SwingConstants.CENTER));
-        conFr.setSize(125, 75);
-        conFr.setVisible(true);
-        start();
-        conFr.dispose();
-        mainFr = new JFrame();
-        mainFr.setSize(800, 600);
-        mainFr.setLayout(new BorderLayout());
-        outF = new JTextArea();
-        outF.setEditable(false);
-        outF.setLineWrap(true);
-        outF.setWrapStyleWord(true);
-        DefaultCaret caret1 = (DefaultCaret) outF.getCaret();
-        caret1.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-        JScrollPane scrollP1 = new JScrollPane(outF);
-        scrollP1.setBounds(0, 0, mainFr.getWidth(), mainFr.getHeight());
-        scrollP1.setPreferredSize(new Dimension(mainFr.getWidth(), 500));
-        mainFr.add(scrollP1, BorderLayout.CENTER);
-        JPanel p = new JPanel(new FlowLayout());
-        inArea = new JTextArea();
-        inArea.setLineWrap(true);
-        inArea.setWrapStyleWord(true);
-        inArea.setMaximumSize(new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE));
-        inArea.getInputMap().put(KeyStroke.getKeyStroke("ENTER"), new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String s = inArea.getText();
-
-                if (s.trim().length() > 0) {
-                    sendStr(s);
-                    outF.append("\nYou: " + s);
-                    inArea.setText(null);
-                }
-            }
-        });
-        inArea.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, InputEvent.SHIFT_DOWN_MASK), new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                inArea.append("\n");
-            }
-        });
-        DefaultCaret caret2 = (DefaultCaret) inArea.getCaret();
-        caret2.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-        final JScrollPane scrollP2 = new JScrollPane(inArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollP2.setBounds(p.getX(), p.getY(), p.getWidth(), p.getHeight());
-        sendMsg = new JButton("send");
-        sendMsg.setEnabled(false);
-        p.add(scrollP2, FlowLayout.LEFT);
-        p.add(sendMsg);
-        mainFr.add(p, BorderLayout.SOUTH);
-        mainFr.setResizable(true);
-        mainFr.setLocationRelativeTo(null);
-        mainFr.setVisible(true);
-        scrollP2.setPreferredSize(new Dimension(mainFr.getWidth() - (sendMsg.getWidth() * 2), inArea.getFont().getSize() * 2));
-        mainFr.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                scrollP2.setPreferredSize(new Dimension(mainFr.getWidth() - (sendMsg.getWidth() * 2), inArea.getFont().getSize() * 2));
-            }
-        });
-        mainFr.addWindowStateListener(new WindowStateListener() {
-            @Override
-            public void windowStateChanged(WindowEvent e) {
-                if (e.getNewState() == JFrame.MAXIMIZED_BOTH || e.getNewState() == JFrame.NORMAL) {
-                    scrollP2.setPreferredSize(new Dimension(mainFr.getWidth() - (sendMsg.getWidth() * 2), inArea.getFont().getSize() * 2));
-                }
-            }
-        });
-
-        mainFr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        KeyStroke escapeKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false);
-        Action escapeAction = new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mainFr.dispatchEvent(new WindowEvent(mainFr, WindowEvent.WINDOW_CLOSING));
-            }
-        };
-        mainFr.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escapeKeyStroke, "ESCAPE");
-        mainFr.getRootPane().getActionMap().put("ESCAPE", escapeAction);
+        try {
+            srvSocket = new DatagramSocket();
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
         running = true;
         connected = true;
-        OutThread = new Thread();
-        OutThread.start();
         InThread = new Thread(new Runnable() {
             @Override
             public void run() {
                 while (running) {
-                    addOut(getStr());
+                    System.out.println(getStr());
                 }
                 OutThread.interrupt();
             }
@@ -345,21 +63,12 @@ public class Client implements Runnable {
         OutThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                sendMsg.setEnabled(true);
-                sendMsg.setAction(new AbstractAction() {
-
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        String s = inArea.getText();
-                        if (s.trim().length() > 0) {
-                            sendStr(s);
-                            outF.append("\nYou: " + s);
-                            inArea.setText(null);
-                        }
-                    }
-                });
-                sendMsg.setText("send");
                 while (running) {
+                    String s = new Scanner(System.in).nextLine();
+                    if (s.trim().length() > 0) {
+                        sendStr(s);
+                        System.out.println("\nYou: " + s);
+                    }
                     try {
                         Thread.sleep(250);
                     } catch (InterruptedException ex) {
@@ -368,6 +77,7 @@ public class Client implements Runnable {
                 InThread.interrupt();
             }
         });
+        OutThread.start();
         InThread.start();
         while (running) {
             try {
@@ -377,12 +87,6 @@ public class Client implements Runnable {
         }
 
         srvSocket.close();
-        myID = "";
-        srvIP = "";
-        srvPort = 9002;
-        if (reset) {
-            reset();
-        }
     }
 
     private void sendObj(Object obj) {
@@ -403,12 +107,12 @@ public class Client implements Runnable {
     private void sendStr(String str) {
         if (str.matches("!quit") || str.matches("!q")) {
             sendStr("(STX)" + "close" + "(ETX)");
-            addOut("You have disconnected!");
+            System.out.println("You have disconnected!");
             reset = false;
             running = false;
         } else if (str.matches("!disconnect") || str.matches("!dc")) {
             sendStr("(STX)" + "close" + "(ETX)");
-            addOut("You have disconnected!");
+            System.out.println("You have disconnected!");
             running = false;
             reset = true;
         } else if (str.matches("!l") || str.matches("!list")) {
@@ -427,17 +131,6 @@ public class Client implements Runnable {
             ObjectInputStream is = new ObjectInputStream(in);
             Object obj = (Object) is.readObject();
             return obj;
-        } catch (SocketException ex) {
-            if (running) {
-                addOut("You have been disconnected from the server!");
-                running = false;
-            }
-            reset = true;
-        } catch (EOFException ex) {
-            if (running) {
-                addOut("You have been disconnected from the server!");
-                running = false;
-            }
         } catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
@@ -463,18 +156,10 @@ public class Client implements Runnable {
         return str;
     }
 
-    private void addOut(String str) {
-        if (outF.getText().isEmpty()) {
-            outF.setText(str);
-        } else {
-            outF.append("\n" + str);
-        }
-    }
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new Client().run();
+        new Client("localhost", 9002, "abcdef").run();
     }
 }
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -1,12 +1,5 @@
 package server;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowEvent;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -16,30 +9,15 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
 import java.net.SocketException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.KeyStroke;
-import javax.swing.SwingConstants;
-import javax.swing.text.DefaultCaret;
 
 /**
  *
@@ -49,8 +27,6 @@ public class Server implements Runnable {
 
     private DatagramSocket srvSocket;
     private int srvPort;
-    private JFrame startFr;
-    private JTextArea inA;
     private final ArrayList<InetSocketAddress> usersL;
     private final Map<InetSocketAddress, Object> toSend;
     private Long srvTime;
@@ -58,30 +34,6 @@ public class Server implements Runnable {
 
     public Server(int port) {
         srvPort = port;
-        startFr = new JFrame();
-        startFr.setResizable(true);
-        startFr.setLocationRelativeTo(null);
-        KeyStroke escapeKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false);
-        Action escapeAction = new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                startFr.dispatchEvent(new WindowEvent(startFr, WindowEvent.WINDOW_CLOSING));
-            }
-        };
-        startFr.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(escapeKeyStroke, "ESCAPE");
-        startFr.getRootPane().getActionMap().put("ESCAPE", escapeAction);
-        startFr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        startFr.setSize(640, 480);
-        startFr.setLayout(new GridLayout());
-        inA = new JTextArea();
-        inA.setEditable(false);
-        inA.setLineWrap(true);
-        inA.setWrapStyleWord(true);
-        DefaultCaret caret1 = (DefaultCaret) inA.getCaret();
-        caret1.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-        JScrollPane scrollP = new JScrollPane(inA);
-        startFr.add(scrollP);
-        startFr.setVisible(true);
         usersL = new ArrayList();
         toSend = new HashMap();
     }
@@ -95,52 +47,15 @@ public class Server implements Runnable {
                 portErr = false;
             } catch (SocketException ex) {
                 portErr = true;
-                final JDialog d = new JDialog(startFr, "", true);
-                d.setLayout(new BorderLayout());
-                JButton but1 = new JButton("OK");
-                JButton but2 = new JButton("EXIT");
-                d.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-                JLabel str = new JLabel("Port already in use! Input a new port: ", SwingConstants.CENTER);
-                final JTextField srvP = new JTextField(Integer.toString(srvPort), 10);
-                but1.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e1) {
-                        String input = srvP.getText();
-                        if (input.matches("[0-9]+")) {
-                            srvPort = Integer.parseInt(input);
-                            d.dispose();
-                        }
-                    }
-                });
-                but2.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e1) {
-                        d.dispose();
-                        startFr.dispatchEvent(new WindowEvent(startFr, WindowEvent.WINDOW_CLOSING));
-                    }
-                });
-                JPanel p1 = new JPanel(new FlowLayout());
-                JPanel p2 = new JPanel(new FlowLayout());
-                JPanel p3 = new JPanel(new FlowLayout());
-                d.setLocationRelativeTo(startFr);
-                d.setResizable(false);
-                p1.add(str);
-                p2.add(srvP);
-                p3.add(but1);
-                p3.add(but2);
-                d.add(p1, BorderLayout.NORTH);
-                d.add(p2, BorderLayout.CENTER);
-                d.add(p3, BorderLayout.SOUTH);
-                d.pack();
-                d.setVisible(true);
+                System.out.print("Port already in use! Input a new port: ");
+                srvPort = new Scanner(System.in).nextInt();
             }
         } while (portErr);
         srvTime = (System.currentTimeMillis() / 1000L);
         srvRunning = true;
-        addOut("Server starting up on UDP port: " + srvPort);
+        System.out.println("Server starting up on UDP port: " + srvPort);
 
         Thread in = new Thread(new Runnable() {
-
             @Override
             public void run() {
                 while (srvRunning) {
@@ -199,14 +114,6 @@ public class Server implements Runnable {
             }
         });
         out.start();
-    }
-
-    private void addOut(String str) {
-        if (inA.getText().isEmpty()) {
-            inA.setText(str);
-        } else {
-            inA.append("\n" + str);
-        }
     }
 
     private void sendObj(Object obj, String ipaddr, int port) {
