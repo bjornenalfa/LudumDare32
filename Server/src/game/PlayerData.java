95ee653aa165c2712279acebbc0e65acb3557574
bjornenalfa <bjorn.detterfelt@telia.com>
2015-07-05 16:09:58 +0200

    player data

---
+++ Server/src/game/PlayerData.java
@@ -0,0 +1,11 @@
+package game;
+
+public class PlayerData {
+    float x;
+    float y;
+    
+    public PlayerData(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ Server/src/game/PlayerDataList.java
@@ -0,0 +1,15 @@
+package game;
+
+import java.util.ArrayList;
+
+public class PlayerDataList {
+    ArrayList<PlayerData> list;
+    
+    public PlayerDataList(ArrayList dataList) {
+        list = dataList;
+    }
+    
+    public PlayerDataList() {
+        list = new ArrayList();
+    }
+}
